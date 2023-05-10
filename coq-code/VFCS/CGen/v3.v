4d9986c7f90a63d3013e383a85cb363c239df501
ZhengPu Shi <zhengpushi@nuaa.edu.cn>
2023-05-10 12:12:59 +0800

    the datatype of array maybe error.

--- coq-code/VFCS/CGen/v3.v
+++ coq-code/VFCS/CGen/v3.v
@@ -34,6 +34,7 @@
   }
  *)
 
+Require Import Matrix.
 Require Import List. Import ListNotations.
 Require Import Strings.String.
 Require Import ZArith.
@@ -100,7 +101,7 @@ Inductive DataValue :=
 | DVbool (b : bool)
 | DVint (i : Z)
 | DVfloat (f : R)
-| DVvec (t : DataType) (n : nat) (v : @vec DataValue n)
+| DVvec (n : nat) (v : @vec DataValue n)
 | DVstruct (s : list DataValue)
 .
 
@@ -110,7 +111,7 @@ Fixpoint DataValueDef (t : DataType) : DataValue :=
   | DTbool => DVbool false
   | DTint => DVint 0
   | DTfloat => DVfloat R0
-  | DTvec t n => DVvec t n (f2v _ n (fun i => DataValueDef t))
+  | DTvec t n => DVvec n (f2v _ n (fun i => DataValueDef t))
   | DTstruct f => DVstruct (map (fun '(s,t) => DataValueDef t) f)
   end.
 
@@ -128,10 +129,62 @@ Section test.
   Compute DVbool true.
   Compute DVint 5.
   Compute DVfloat R1.
-  Compute DVvec DTint 3 (l2v _ 3 (DataValueDef DTint) [DVint 1; DVint 2; DVint 3]).
-  Compute DVstruct [DVint 3;DVfloat R1].
+  (* 这里，使用了相同的数据类型 Dvint *)
+  Compute DVvec 3 (l2v _ 3 (DataValueDef DTint) [DVint 1; DVint 2; DVint 3]).
+  (* 但是，还是可以构造出不同类型上的数组，这不符合数组的语义。例如 *)
+  Compute DVvec 3 (f2v _ _ (fun i =>
+                              match i with
+                              | 0 => DVint 3
+                              | _ => DVfloat 2
+                              end)).
   
+  Compute DVstruct [DVint 3;DVfloat R1].
 End test.
+
+(* 基于AST的基本实矩阵理论 *)
+Module RMAT.
+
+  (* AST上的mat，简称 M *)
+  Open Scope R.
+  
+  (* 使用嵌套列表创建矩阵 *)
+  Definition l2M (r c : nat) (dl : list (list R)) :=
+    f2v DataValue r
+    (fun i => DVvec c
+              (f2v DataValue c
+                 (fun j => DVfloat (nth j (nth i dl []) R0)))).
+
+  (* 将 mat 转换为 M *)
+  Definition m2M (r c : nat) (m : @mat R r c) :=
+    f2v DataValue r
+    (fun i => DVvec c
+              (f2v DataValue c
+                 (fun j => DVfloat (m$i$j)))).
+
+
+  (* 将任意 DataValue 转换为矩阵，只有 vec (vec R) 才认可 *)
+  Check fun (d : DataValue) =>
+          match d with
+          | DVvec r v =>
+              let '(mk_vec _ _ f) := v in
+              (* match  with *)
+              (* | DVvec c v' => Some c *)
+              (* |_ => Some 0 *)
+          (* end *)
+              Some r
+          | _ => None
+          end.
+  Definition M2m (d : DataValue) (r c : nat) : option (@mat R r c).
+    destruct d.
+    (* 4:{ inversion v. *)
+    (* :  *)
+    Abort.
+
+  (* 矩阵加法 *)
+  
+  (** 3x3 矩阵 *)
+  
+End RMAT.  
 
 (* 路径，左右值都可能用到 *)
 (* Inductive Path := *)
